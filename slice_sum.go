d766a523f5e8349ce59506ecbf84874827c8c018
Gonewee <79505175+Gonewee@users.noreply.github.com>
2021-03-12 14:31:32 +0800

    Update slice_sum.go

--- slice_sum.go
+++ slice_sum.go
@@ -1,5 +1,15 @@
 package main
 
 func main()  {
-	
+	items := []int{10, 20, 30, 40, 50}
+	fmt.Println(items)
+	sum := sum(items...)
+	fmt.Printf("The sum of items is equal to %d.\n", sum)	
+}
+func sum(items ...int) int {
+	sum := 0
+	for _, item := range items {
+		sum += item
+	}
+	return sum
 }
